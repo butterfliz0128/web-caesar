8f058f70b955d5fed4a2a9efdde2be648dbfd2b4
Teresa Anyan <tjanyan@yahoo.com>
2018-03-24 16:29:57 -0500

    send results back to screen

--- main.py
+++ main.py
@@ -5,12 +5,13 @@ app = Flask(__name__)
 app.config['DEBUG'] = True
 
 
+
 form = """
 <!DOCTYPE html>
 <html>
     <head>
         <style>
-            form {
+            form {{
 
                 background-color: #eee;
                 padding: 20px;
@@ -18,27 +19,27 @@ form = """
                 width: 540px;
                 font: 16px sans-serif;
                 border-radius: 10px;
-            }
+            }}
 
-            textarea {
+            textarea {{
                 margin: 10px 0;
                 width: 540px;
                 height: 120px;
-            }
+            }}
 
         </style>
     </head>
     <body>
-        <form action="/encrypt" method="post">
+        <form action="/" method="post">
         <label for="rot">
             Rotate By:
-            <input type="text" id="rot" name="rot" value="0"/>
+            <input type="text" id="rot" name="rot" value=0>
         </label>
         <br>
         <br>
 
-        <input type="textarea" id="text" name="text"/>
-
+         <textarea type="text" name="text">{0}</textarea>
+    
         <br>
         <br>
         <input type="submit" value="Submit Query"/>
@@ -54,15 +55,17 @@ form = """
 @app.route("/")
 def index():
     #return "Hello World"
-    return form 
+    #return form
+    return form.format("")
     
-@app.route("/encrypt", methods=['POST'])
+@app.route("/", methods=['POST'])
 def encrypt():
     rot_num = int(request.form['rot'])
     form_text = request.form['text']
     convert_string = rotate_string(form_text, rot_num)
 
-    return "<h1>" + convert_string + "</h1>"
+    #return "<h1>" + convert_string + "</h1>"
+    return form.format(convert_string)
 
 
 app.run()
